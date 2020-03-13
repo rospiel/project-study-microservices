e9416592061053d262f5cc41c3a35ad1191ab795
Rodrigo Santos <rospielberg@gmail.com>
2020-03-13 09:45:39 -0300

    Basic structure

---
+++ study-application/src/main/java/com/study/microservices/studyapplication/StudyApplication.java
@@ -0,0 +1,13 @@
+package com.study.microservices.studyapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyApplication.class, args);
+	}
+
+}
