301a65a94ded2df16f46ca0d4c1d649073e30be9
Rodrigo Santos <rospielberg@gmail.com>
2020-03-13 14:38:57 -0300

    Add properties to access database and registry of new service on eureka

--- study-application/src/main/java/com/study/microservices/studyapplication/StudyApplication.java
+++ study-application/src/main/java/com/study/microservices/studyapplication/StudyApplication.java
@@ -2,8 +2,10 @@ package com.study.microservices.studyapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class StudyApplication {
 
 	public static void main(String[] args) {
