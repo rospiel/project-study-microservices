2981377fcdd842f5afd564c4387d31842b1a2281
rospiel <rospielberg@gmail.com>
2021-09-30 13:52:18 -0300

    Ajuste em test e dockerizando o configserver

--- study-gateway-server/src/test/java/com/study/microservices/studygatewayserver/StudyGatewayServerApplicationTests.java
+++ study-gateway-server/src/test/java/com/study/microservices/studygatewayserver/StudyGatewayServerApplicationTests.java
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
-class StudyGatewayServerApplicationTests {
+public class StudyGatewayServerApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
